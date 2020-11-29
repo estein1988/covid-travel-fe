6a6c23124cd2893b363fc1c1d6ab8e136f5f720b
estein1988 <estein1988@gmail.com>
2020-11-28 18:00:27 -0700

    Change map pop-up window text.

--- src/components/MarkerPopup.js
+++ src/components/MarkerPopup.js
@@ -5,9 +5,8 @@ const MarkerPopup = ({data}) => {
     return  (
     <Popup>
         <div className='popup-text'>
-            <h2>{data.country_name}</h2>
-            <h4>Current Status: {data.current_status.charAt(0).toUpperCase() + data.current_status.slice(1)}</h4>
-            <p>{data.days_to_quarantine} days required to quarantine</p>
+            <h2>{data.country_name}: {data.current_status.charAt(0).toUpperCase() + data.current_status.slice(1)}</h2>
+            <h5>{data.days_to_quarantine} days required to quarantine</h5>
             <p>Exceptions: {data.closed_exceptions.join('; ')}</p>
             <p>Notes: {data.international_travel_note}</p>
         </div>
